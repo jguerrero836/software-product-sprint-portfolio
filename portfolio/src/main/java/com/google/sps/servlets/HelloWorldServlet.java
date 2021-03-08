d8e154536b20dc605013bea477f86cc92ee332b7
Jennifer <jenniferguerrero836@gmail.com>
2021-03-08 22:16:32 +0000

    Added arraylist/array of string messages for get json request

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -5,14 +5,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+
+
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world again!</h1>");
+    //response.setContentType("text/html;");
+    //response.getWriter().println("<h1>Hello world again!</h1>");
+
+    String[] arr = {"hello", "bonjour", "hola"};
+    ArrayList<String> arr_list = new ArrayList<String>();
+    arr_list.add("hello");
+    arr_list.add("hello2");
+    arr_list.add("hello3");
+    Gson gson = new Gson();
+    String json = gson.toJson(arr_list);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+
   }
+  
 }
